599f3cc78e98eb35d933d6af52f91772bb6c2edd
RiyoRaj18 <59334045+RiyoRaj18@users.noreply.github.com>
2021-01-22 15:46:19 +0530

    Basic setup for Task 1

---
+++ Tsak_1/main.js
@@ -0,0 +1,6 @@
+var input=document.getElementById("input");
+console.log(input);
+var submit=document.getElementById("submit");
+console.log(submit);
+var output=document.getElementById("output");
+console.log(output);
