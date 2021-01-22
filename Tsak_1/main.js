4e05c8eccd43109d25fb54476b6eb916223b078e
RiyoRaj18 <59334045+RiyoRaj18@users.noreply.github.com>
2021-01-22 22:10:00 +0530

    Logic Complete for task 1

--- Tsak_1/main.js
+++ Tsak_1/main.js
@@ -1,6 +1,37 @@
-var input=document.getElementById("input");
-console.log(input);
 var submit=document.getElementById("submit");
-console.log(submit);
 var output=document.getElementById("output");
-console.log(output);
+function submmit(){
+    var input=document.getElementById("input").value;
+    if(input == ""){
+        alert("You Need To Type Some String.....");
+    } else {
+       input= input.toLowerCase();
+       input= input.split("");
+         
+         var count=0;
+            function callMe(){
+            var iterateMe=false;
+                for(var i=count;i<input.length;i++){
+                    if(input[count]==input[i+1]){
+                    if((i+1)-count==1){
+                        count++;
+                    }
+                    iterateMe=true;
+                    break;
+                    }
+                }
+                if(iterateMe){
+                count++;
+                callMe();
+                }else{
+                    if(count==input.length){
+                    output.innerHTML="-1";
+                    }else{
+                    output.innerHTML=count;
+                }
+                }
+            }
+            callMe()
+    }
+}
+
