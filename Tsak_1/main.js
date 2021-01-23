8ce5b55eed4e1da86c6acbb738ab8cbe72edfb69
RiyoRaj18 <59334045+RiyoRaj18@users.noreply.github.com>
2021-01-23 10:44:12 +0530

    Logic Complete for task 1 and basic Setup for Task2

--- Tsak_1/main.js
+++ Tsak_1/main.js
@@ -3,35 +3,21 @@ var output=document.getElementById("output");
 function submmit(){
     var input=document.getElementById("input").value;
     if(input == ""){
-        alert("You Need To Type Some String.....");
+        alert("You Need To Type Some input.....");
     } else {
        input= input.toLowerCase();
-       input= input.split("");
-         
-         var count=0;
-            function callMe(){
-            var iterateMe=false;
-                for(var i=count;i<input.length;i++){
-                    if(input[count]==input[i+1]){
-                    if((i+1)-count==1){
-                        count++;
-                    }
-                    iterateMe=true;
-                    break;
-                    }
-                }
-                if(iterateMe){
-                count++;
-                callMe();
-                }else{
-                    if(count==input.length){
-                    output.innerHTML="-1";
-                    }else{
-                    output.innerHTML=count;
-                }
+    //    input= input.split("");
+    output.innerHTML= firstNonRepeatedCharacter(input);
+        function firstNonRepeatedCharacter(string) {
+            for (var i = 0; i < string.length; i++) {
+                var c = string.charAt(i);
+                if (string.indexOf(c) == i && string.indexOf(c, i + 1) == -1) {
+                    return i;
                 }
             }
-            callMe()
+            return -1;
+        
+        }
     }
 }
 
