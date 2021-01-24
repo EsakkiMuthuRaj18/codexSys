740c38f71e871b48d5999569d65db1bdca5f2070
RiyoRaj18 <59334045+RiyoRaj18@users.noreply.github.com>
2021-01-24 11:47:15 +0530

    fix complete task 2

---
+++ Task_2/main.js
@@ -0,0 +1,38 @@
+var result=document.getElementById('result');
+var buttons=Array.from(document.getElementsByClassName('button'));
+
+buttons.map(button =>{
+    button.addEventListener('click',(e) =>{
+        switch(e.target.innerText){
+            case 'AC':
+                result.innerText = '';
+                break;
+            case 'DELETE':
+                if(result.innerText){
+                    result.innerText = result.innerText.slice(0,-1);
+                }
+                break;
+            case '=':
+                try{
+                    result.innerText = eval(result.innerText).toFixed(15);
+                }catch{
+                    result.innerText = 'Invalid Expression';
+                }
+                break;
+                case '%':
+                    try{
+                        result.innerText = percentage(result.innerText);
+                    }catch{
+                        result.innerText = 'Invalid Expression';
+                    }
+                    break;
+            default:
+                result.innerText += e.target.innerText;
+        }
+    });
+});
+function percentage(num)
+{
+  return (num/100);
+}
+          
